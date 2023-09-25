98b3a0fa8b7e798fa2dc9477b7881892aa655768
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-09-25 09:15:35 +0530

    Created context classes and database connection

---
+++ CompanyEmployees/Repository/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Company>? Companies { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+    }
+}
