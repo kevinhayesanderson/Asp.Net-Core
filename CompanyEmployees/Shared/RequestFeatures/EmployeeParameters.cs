53087c4bcc8f22796cb3ab1e5f20780e4cc9f54c
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-12-07 22:25:54 +0530

    Added Sorting

--- CompanyEmployees/Shared/RequestFeatures/EmployeeParameters.cs
+++ CompanyEmployees/Shared/RequestFeatures/EmployeeParameters.cs
@@ -2,6 +2,10 @@
 {
     public class EmployeeParameters : RequestParameters
     {
+        public EmployeeParameters()
+        {
+            OrderBy = "name";
+        }
         public uint MinAge { get; set; }
         public uint MaxAge { get; set; } = int.MaxValue;
         public bool ValidAgeRange => MaxAge > MinAge;
