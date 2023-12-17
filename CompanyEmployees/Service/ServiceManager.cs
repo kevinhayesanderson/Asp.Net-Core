26740c0eb2480e3adde23533f52038f94ffdbea5
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-12-17 10:49:36 +0530

    cleanup

--- CompanyEmployees/Service/ServiceManager.cs
+++ CompanyEmployees/Service/ServiceManager.cs
@@ -4,24 +4,22 @@ using Service.Contracts;
 
 namespace Service
 {
-    public sealed class ServiceManager(IRepositoryManager repositoryManager, IMapper mapper, IEmployeeLinks employeeLinks) : IServiceManager
+    public sealed class ServiceManager : IServiceManager
     {
-        private readonly Lazy<ICompanyService> _companyService =
-            new(
+        private readonly Lazy<ICompanyService> _companyService;
+        private readonly Lazy<IEmployeeService> _employeeService;
+
+        public ServiceManager(IRepositoryManager repositoryManager, IMapper mapper, IEmployeeLinks employeeLinks)
+        {
+            _companyService = new(
                 () => new CompanyService(repositoryManager, mapper),
                 LazyThreadSafetyMode.PublicationOnly);
-        private readonly Lazy<IEmployeeService> _employeeService =
-            new(
+            _employeeService = new(
                 () => new EmployeeService(repositoryManager, mapper, employeeLinks),
                 LazyThreadSafetyMode.PublicationOnly);
-
-        public ICompanyService CompanyService
-        {
-            get
-            {
-                return _companyService.Value;
-            }
         }
+
+        public ICompanyService CompanyService => _companyService.Value;
 
         public IEmployeeService EmployeeService => _employeeService.Value;
     }
